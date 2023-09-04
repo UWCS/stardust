f914d98b7fbe5c72f9ff752e3b8259050926d284
ericthelemur <owenconnors1@gmail.com>
2023-09-04 01:24:37 +0100

    Chmod not longer necessary

--- build.sh
+++ build.sh
@@ -7,10 +7,8 @@ git pull --recurse-submodules
 # Build draft
 sed -i 's/# DRAFT//g' config.toml
 ./zola build --drafts --base-url https://draft.uwcs.co.uk --output-dir ../draft --force
-chmod -R 755 draft
 
 git restore config.toml
 
 # Build main
 ./zola build --base-url https://new.uwcs.co.uk --output-dir ../public --force
-chmod -R 755 public
