f3c73dc2a679a209543df19e8ae1f2db46de34db
ericthelemur <owenconnors1@gmail.com>
2023-09-04 01:10:54 +0100

    Change build script to correct directories

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ git pull --recurse-submodules
 
 # Build draft
 sed -i 's/# DRAFT//g' config.toml
-./zola build --drafts --base-url https://draft.uwcs.co.uk --output-dir draft --force
+./zola build --drafts --base-url https://draft.uwcs.co.uk --output-dir ../draft --force
 chmod -R 755 draft
 
 git restore config.toml
 
 # Build main
-./zola build --base-url https://new.uwcs.co.uk
+./zola build --base-url https://new.uwcs.co.uk --output-dir ../public --force
 chmod -R 755 public
