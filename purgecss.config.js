a4b0b17eb67b68ad59c966436d9bf5dc525ebab6
Mole1424 <joel@joelcoulon.com>
2025-01-11 20:32:54 +0000

    updated css config

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ['*.html', '**/*.html', '*.js', '**/*.js'],
   css: ['*.css'],
   output: ['.'],
-  dynamicAttributes: ["data-bs-theme", "aria-expanded"],
-  safelist: [/day-2/, /day-3/, /show/, 'a'],
-}
+  dynamicAttributes: ["data-bs-theme", "aria-expanded", "data-bs-toggle"],
+  safelist: [
+    "dropdown", "dropdown-menu", "dropdown-item", "dropdown-toggle",
+    "nav-item", "nav-link", "me-lg-2",
+    {
+      pattern: /a\[href\^="https:\/\/"\]:not/,
+      variants: ["after"]
+    },
+    /day-2/, /day-3/
+  ]
+};
