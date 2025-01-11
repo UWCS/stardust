cc1fff961392857edc856d3a61699823a5611839
Mole1424 <joel@joelcoulon.com>
2025-01-11 20:42:29 +0000

    better regex pattern matching

--- purgecss.config.js
+++ purgecss.config.js
@@ -6,10 +6,7 @@ module.exports = {
   safelist: [
     "dropdown", "dropdown-menu", "dropdown-item", "dropdown-toggle",
     "nav-item", "nav-link", "me-lg-2",
-    {
-      pattern: /a\[href\^="https:\/\/"\]:not/,
-      variants: ["after"]
-    },
-    /day-2/, /day-3/
+    { pattern: /^a\[href\^="https:\/\/"\]:not\(/ },
+    { pattern: /day-\d/ },
   ]
 };
