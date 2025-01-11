361195ae4694609a08ddc3da7c4163cf0d685a73
Mole1424 <joel@joelcoulon.com>
2025-01-11 21:06:56 +0000

    removed {pattern ...}

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,11 +2,6 @@ module.exports = {
   content: ['*.html', '**/*.html', '*.js', '**/*.js'],
   css: ['*.css'],
   output: ['.'],
-  dynamicAttributes: ["data-bs-theme", "aria-expanded", "data-bs-toggle"],
-  safelist: [
-    "dropdown", "dropdown-menu", "dropdown-item", "dropdown-toggle",
-    "nav-item", "nav-link", "me-lg-2",
-    { pattern: /^a\[href\^="https:\/\/"\]:not\(/ },
-    { pattern: /day-\d/ },
-  ]
+  dynamicAttributes: ['data-bs-theme', 'aria-expanded', 'data-bs-toggle'],
+  safelist: [/day-2/, /day-3/, /show/, 'a', 'dropdown', 'dropdown-menu', 'dropdown-item', 'dropdown-toggle', 'nav-item', 'nav-link', 'me-lg-2'],
 };
