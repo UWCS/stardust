15f2f1427dc62ad6e8a991c61bf8f00da61b4794
ericthelemur <owenconnors1@gmail.com>
2025-01-14 22:09:00 +0000

    Fix? navbar dropdown

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,6 +2,6 @@ module.exports = {
   content: ['*.html', '**/*.html', '*.js', '**/*.js'],
   css: ['*.css'],
   output: ['.'],
-  dynamicAttributes: ['data-bs-theme', 'aria-expanded', 'data-bs-toggle'],
+  dynamicAttributes: ['data-bs-theme', 'aria-expanded', 'data-bs-toggle', 'data-bs-popper'],
   safelist: [/day-2/, /day-3/, /show/, 'a', 'dropdown', 'dropdown-menu', 'dropdown-item', 'dropdown-toggle', 'nav-item', 'nav-link', 'me-lg-2'],
 };
