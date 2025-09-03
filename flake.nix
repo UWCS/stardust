76deef9870b5b533b5a131deb4ba448d3071f610
raven0034 <monkeyboy2805@gmail.com>
2025-09-03 06:24:46 +0100

    Bump for nix

--- flake.nix
+++ flake.nix
@@ -1,38 +1,52 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
-
-      perSystem =
-        { pkgs, lib, ... }: rec {
-          
-          packages.default = pkgs.rustPlatform.buildRustPackage rec {
-            pname = "zola";
-            version = "1.0.0";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "ericthelemur";
-              repo = pname;
-              rev = "453b92af84ca653125423f8c0e4405842e140ca1";
-              hash = "sha256-5RlfNwuCbon63SygVmMKqvmStTGQnsAouxE5IjZMejw=";
+  outputs = { self, nixpkgs, utils, rust-overlay }:
+    utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs {
+                inherit system;
+                overlays = [ rust-overlay.overlays.default ];
+            };
+  
+            rustToolchain = pkgs.rust-bin.stable."1.87.0".default;
+  
+            rustPlatform = pkgs.makeRustPlatform {
+                cargo = rustToolchain;
+                rustc = rustToolchain;
             };
 
-            cargoLock = {
-              lockFile = "${src}/Cargo.lock";
-              allowBuiltinFetchGit = true;
-            };
-          };
+            zolaPkg = rustPlatform.buildRustPackage rec {
+                pname = "zola";
+                version = "0.21.0-rev1";
+                
+                src = pkgs.fetchFromGitHub {
+                    owner = "raven0034";
+                    repo = pname;
+                    rev = "cf3b50222c27a06389df08c3f4344cb9112fdc60";
+                    hash = "sha256-k2Q6XHWkTMuVlv5pjcYWnGql+K96U/Q6wOZpQz4RZvs=";
+                };
 
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [ packages.default ];
-          };
-        };
-    };
+                cargoLock = {
+                    lockFile = "${src}/Cargo.lock";
+                    allowBuiltinFetchGit = true;
+                };
+            };
+        in {
+            packages.default = zolaPkg;
+
+            devShells.default = pkgs.mkShell {
+                buildInputs = [ zolaPkg pkgs.python313 ];
+                shellHook = ''
+                    mkdir -p ./bin
+                    ln -sf ${zolaPkg}/bin/zola ./bin/zola
+                    echo "Linked ./bin/zola -> ${zolaPkg}/bin/zola"
+                '';
+            };
+        }
+    );
 }
